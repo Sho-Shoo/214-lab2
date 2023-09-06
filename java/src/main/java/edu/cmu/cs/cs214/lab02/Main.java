bb21a25080f1ccb3c4637c44f742292b973de5e3
Tianze Shou <shoutianze@outlook.com>
2023-09-06 10:09:37 -0400

    Implement Java Shape interface and encapsulation for Circle, Rectangle, and Square classes

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,22 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(10);
+        Shape square = new Square(5);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer renderer1 = new Renderer(rectangle);
+        Renderer renderer2 = new Renderer(circle);
+        Renderer renderer3 = new Renderer(square);
 
-        renderer.draw();
+        renderer1.draw();
+        renderer2.draw();
+        renderer3.draw();
     }
 }
