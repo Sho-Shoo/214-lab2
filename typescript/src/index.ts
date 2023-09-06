807d30825720cc5f7f3d2c9d5c4a08718042dec7
Tianze Shou <shoutianze@outlook.com>
2023-09-06 10:25:02 -0400

    Implement Shape interface in TS and corresponding implementation classes

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,16 @@
 import { newRenderer } from "./renderer.js"
-import { Rectangle, newRectangle } from "./shapes/rectangle.js";
+import { newRectangle } from "./shapes/rectangle.js";
+import { newCircle } from "./shapes/circle.js";
+import { newSquare } from "./shapes/square.js";
+import { Shape } from "./shapes/shape.js";
 
-const rectangle: Rectangle = newRectangle(2, 3)
-const renderer = newRenderer(rectangle)
-renderer.draw();
+const rectangle: Shape = newRectangle(2, 3);
+const circle: Shape = newCircle(3);
+const square: Shape = newSquare(10);
+const renderer1 = newRenderer(rectangle);
+const renderer2 = newRenderer(circle);
+const renderer3 = newRenderer(square);
+
+renderer1.draw();
+renderer2.draw();
+renderer3.draw();
